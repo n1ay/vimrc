43ba1c312fb9c337bf7c3bb3b78c4e397ab80f21
kamil <kammmilkos@gmail.com>
2019-04-19 12:01:12 +0200

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curr_dir="$(pwd)"
+[ -e ~/.vim/bundle/Vundle.vim ] || (echo 'Installing Vundle, the vim addon manager...'; git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim)
+
+cp .vimrc ~/.vimrc
+echo 'Installing vim plugins with Vundle...'
+vim +PluginInstall +qall
+
+echo 'Setting up YCM...'
+ycm_deps='build-essential cmake python3-dev'
+
+dpkg -l $ycm_deps || (echo "Please provide a password to install $ycm_deps"; sudo apt install -y $ycm_deps || (echo "Can't setup YCM without requested requirements"; exit 1))
+cd ~/.vim/bundle/YouCompleteMe
+python3 ./install.py --clang-completer && cd "$curr_dir"
+
